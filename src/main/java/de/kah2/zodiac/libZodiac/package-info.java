d3d1f175cf2e9ec30c57414d42f6ed45312dbf7f
kahles <st-git@kah2.de>
2019-07-02 00:07:00 +0200

    update docs

--- src/main/java/de/kah2/zodiac/libZodiac/package-info.java
+++ src/main/java/de/kah2/zodiac/libZodiac/package-info.java
@@ -1,8 +1,13 @@
 /**
  * This package contains the main classes of this framework and three
- * sub-packages:
+ * sub-packages. Here the most important elements:
  * @see de.kah2.zodiac.libZodiac.Calendar - the main class of this framework. It initialized days and triggers their data calculation.
  * @see de.kah2.zodiac.libZodiac.Day - the "glue" between the other classes.
+ * @see de.kah2.zodiac.libZodiac.DayStorableDataSet - A basic class to extend with serialisation functionality.
+ * @see de.kah2.zodiac.libZodiac.CalendarGenerator - does the work of handling calculation threads. Allows tweaking thread priority and
+ * number of threads.
+ * @see de.kah2.zodiac.libZodiac.ProgressListener - implementations of this class can be registered at
+ * {@link de.kah2.zodiac.libZodiac.Calendar} to receive progress updates
  * @see de.kah2.zodiac.libZodiac.planetary.PlanetaryDayData - calculates and contains the basic data needed for ...
  * @see de.kah2.zodiac.libZodiac.zodiac - contains enums and classes to manage planetary and zodiac data
  * @see de.kah2.zodiac.libZodiac.interpretation - contains classes needed for interpreting the raw data.
